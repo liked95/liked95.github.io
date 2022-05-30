3712ecdbd39992f73b6bf037726dc79b091c8842
liked95 <hoaison95.ftu@gmail.com>
2022-05-30 16:24:00 +0700

    funoflearning web

---
+++ FunLearning/main.js
@@ -0,0 +1,39 @@
+// const body = document.getElementById('body')
+// const menuButton = document.querySelector('.fa-solid.fa-bars')
+// const navContainer = document.querySelector('.nav-container')
+// const sideNav = document.querySelector('.side-nav')
+// const closeButton = document.querySelector('.menu-close')
+
+// menuButton.addEventListener('click', () => {
+//     sideNav.classList.add('active');
+//     navContainer.classList.add('active');
+
+//     // sideNav.classList.remove('fade');
+//     // navContainer.classList.remove('fade');
+    
+//     body.style.overflowY = `hidden`;
+    
+// })
+
+// function closeTheMenu() {
+//     sideNav.classList.remove('active');
+//     navContainer.classList.remove('active');
+
+//     // sideNav.classList.add('fade');
+//     // navContainer.classList.add('fade');
+
+//     body.style.overflowY = `auto`;
+// }
+
+// closeButton.addEventListener('click', (e) => {
+//     e.stopPropagation();
+//     closeTheMenu();
+// })
+
+
+// document.addEventListener('click', (e) => {
+//     e.stopPropagation();
+//     if (e.target.classList.contains('nav-container')) {
+//         closeTheMenu();
+//     }
+// })
