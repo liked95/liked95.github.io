f8f09699b778da14729562a607aeb8e07c2b8f6a
liked95 <101644745+liked95@users.noreply.github.com>
2022-07-29 18:23:42 +0700

    deleteItem OK

--- FinalProject-TechGalaxy/static/js/shopping-cart.js
+++ FinalProject-TechGalaxy/static/js/shopping-cart.js
@@ -1,3 +1,5 @@
+renderCart()
+
 function renderCart() {
     const cartTitleEl = document.querySelector(".cart-detail .cart-title")
     let isChecked = getObjectFromLocalStorage("isCheckAll")[sessionID]
@@ -13,54 +15,57 @@ function renderCart() {
     const cartContentEl = document.querySelector(".cart-detail .cart-content")
 
     let cart = getObjectFromLocalStorage("techCart")
-    if (!cart || !cart[sessionID]) {
+    if (!cart || !cart[sessionID] || cart[sessionID].length == 0) {
         cartContentEl.innerHTML = `<p>Bạn chưa mua sản phẩm nào. Tiếp tục mua và quay lại đây nhé </p>`
-        return;
+        // return;
+    } else {
+        let items = cart[sessionID]
+        // render cart detail
+        cartContentEl.innerHTML = ""
+        for (let item of items) {
+            cartContentEl.innerHTML += `
+                <div class="cart-item">
+                    <input type="checkbox" ${item.checked ? "checked" : ""} class="choose-item" onclick="toggleCheck(${item.id}, '${item.alterOption}', '${item.color}')"  />
+    
+                    <a href="./detail.html?id=${item.id}" class="product">
+                      <div class="product-name">${item.name}</div>
+                      <div class="product-attr">(${item.color}, ${item.alterOption})</div>
+                    </a>
+    
+                    <div class="prices">
+                      <div class="new-price">${formatMoney(item.price)}</div>
+                      <div class="old-price">${formatMoney(item.oldPrice)}</div>
+                    </div>
+    
+                    <div class="quantity">
+                      <div class="value-button" id="decrease" value="Decrease Value" onclick="decreaseItemCount(${item.id}, '${item.alterOption}', '${item.color}')">-</div>
+                      <span>${item.count}</span>
+                      <div class="value-button" id="increase" value="Increase Value" onclick="increaseItemCount(${item.id}, '${item.alterOption}', '${item.color}')">+</div>
+                    </div>
+    
+                    <div class="value">${formatMoney(item.count * item.price)}</div>
+    
+                    <div class="delete-item">
+                      <i class="fa-solid fa-circle-xmark" onclick="deleteItem(${item.id}, '${item.alterOption}', '${item.color}')"></i>
+                    </div>
+                </div>`
+        }
     }
 
-    let items = cart[sessionID]
-    // render cart detail
-    cartContentEl.innerHTML = ""
-    for (let item of items) {
-        cartContentEl.innerHTML += `
-            <div class="cart-item">
-                <input type="checkbox" ${item.checked ? "checked" : ""} class="choose-item" onclick="toggleCheck(${item.id}, '${item.alterOption}', '${item.color}')"  />
 
-                <div class="product">
-                  <div class="product-name">${item.name}</div>
-                  <div class="product-attr">(${item.color}, ${item.alterOption})</div>
-                </div>
-
-                <div class="prices">
-                  <div class="new-price">${formatMoney(item.price)}</div>
-                  <div class="old-price">${formatMoney(item.oldPrice)}</div>
-                </div>
-
-                <div class="quantity">
-                  <div class="value-button" id="decrease" value="Decrease Value" onclick="decreaseItemCount(${item.id}, '${item.alterOption}', '${item.color}')">-</div>
-                  <span>${item.count}</span>
-                  <div class="value-button" id="increase" value="Increase Value" onclick="increaseItemCount(${item.id}, '${item.alterOption}', '${item.color}')">+</div>
-                </div>
-
-                <div class="value">${formatMoney(item.count * item.price)}</div>
-
-                <div class="delete-item">
-                  <i class="fa-solid fa-circle-xmark"></i>
-                </div>
-            </div>`
-    }
 
     // render tien hang va tong so tien
     // chỉ tính tổng tiền item đc check
+    let items = cart[sessionID]
     let totalValue = 0
     for (let item of items) {
+        console.log(item)
         if (item.checked) totalValue += item.count * item.price
     }
     $(".payment .total-value span:last-child").html(formatMoney(totalValue))
 
 }
 
-renderCart()
 
 // Tăng / Giảm số lượng từng item
 function increaseItemCount(id, alterOption, color) {
@@ -96,38 +101,7 @@ function toggleCheck(id, alterOption, color) {
 }
 
 // toggle all checks
-// document.getElementById("toggle-all").addEventListener("click", (e) => {
-//     console.log("start")
-//     let cart = getObjectFromLocalStorage("techCart")
-//     if (!cart) return
-//     let items = cart[sessionID]
-//     if (!items) return
-
-//     if (e.currentTarget.checked) {
-//         for (let item of items) {
-//             item.checked = true
-//         }
-//     } else {
-//         for (let item of items) {
-//             item.checked = false
-//         }
-//     }
-
-//     saveToLocalStorage("techCart", cart)
-
-//     // toggle checkAll for that particular sessionID
-//     let isCheckAll = getObjectFromLocalStorage("isCheckAll")
-//     // console.log(isCheckAll[sessionID])
-//     isCheckAll[sessionID] = !isCheckAll[sessionID]
-//     saveToLocalStorage("isCheckAll", isCheckAll)
-
-//     renderCart()
-//     console.log("end")
-// })
-
-
 function toggleAllChecks(ele) {
-    console.log("start")
     let cart = getObjectFromLocalStorage("techCart")
     if (!cart) return
     let items = cart[sessionID]
@@ -152,6 +126,29 @@ function toggleAllChecks(ele) {
     saveToLocalStorage("isCheckAll", isCheckAll)
 
     renderCart()
+}
+
+// delete Item button
+
+function deleteItem(id, alterOption, color) {
+    console.log(id, alterOption, color)
+    let cart = getObjectFromLocalStorage("techCart")
+    if (!cart) return
+    let items = cart[sessionID]
+    if (!items) return
+    console.log(items)
+
+    // remove the check status in the local storage
+    // let item = items.find(i => i.id == id && i.alterOption == alterOption && i.color == color)
+    // item.checked = false
+
+    // this is pass by value
+    items = items.filter(i => i.id != id || i.alterOption != alterOption || i.color != color)
+    cart[sessionID] = items
+
+    saveToLocalStorage("techCart", cart)
+    renderCart()
+
     console.log("end")
 }
 
