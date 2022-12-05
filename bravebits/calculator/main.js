d7a38fb0506475d147ce7461ab5dda53536187be
liked95 <101644745+liked95@users.noreply.github.com>
2022-12-05 15:48:47 +0700

    decimal + minus sign

--- bravebits/calculator/main.js
+++ bravebits/calculator/main.js
@@ -6,6 +6,7 @@ const divideBtn = document.getElementById("divide")
 const multiplyBtn = document.getElementById("multiply")
 const addBtn = document.getElementById("add")
 const subtractBtn = document.getElementById("subtract")
+const decimalBtn = document.getElementById("decimal")
 const calcBtn = document.getElementById("calc_action")
 
 const [key0, key1, key2, key3, key4, key5, key6, key7, key8, key9] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -17,6 +18,8 @@ let resText = "0";
 let prevOperator
 let operator;
 let count = 0;
+let isDecimalPressed = false;
+let isLastPressCalc = false
 
 function renderResult() {
     resultEle.textContent = resText;
@@ -28,6 +31,9 @@ function resetOperators() {
     })
     prevOperator = operator
     operator = undefined
+    isDecimalPressed = false;
+    isLastPressCalc = false
+
 }
 
 function calcRes() {
@@ -54,8 +60,20 @@ function calcRes() {
         resultEle.textContent = resText;
         resetOperators()
         // console.log("successfully")
+        isLastPressCalc = true
 
     }
+}
+
+function handleResetCalculator() {
+    resText = "0"
+    res = 0;
+    prevRes = undefined;
+    count = 0;
+
+
+    resetOperators()
+    renderResult()
 }
 
 
@@ -67,16 +85,20 @@ Array.from([key0, key1, key2, key3, key4, key5, key6, key7, key8, key9]).forEach
         num = +num.slice(-1)
 
         // skip if press 0 when res value == 0
-        if (num == 0 && resText == 0) {
+        if (num == 0 && resText == 0 && !operator) {
             // console.log("stop");
             return
         }
 
+        if (isLastPressCalc) {
+            handleResetCalculator()
+        }
+
 
         resText = resText == "0" ? num.toString() : resText + num.toString()
-        res = res == 0 ? num.toString() : res.toString() + num.toString()
-        res = +res
-        // console.log(res)
+        res = parseFloat(resText)
+        console.log(res, resText)
+        isLastPressCalc = false
         renderResult()
 
     })
@@ -84,18 +106,13 @@ Array.from([key0, key1, key2, key3, key4, key5, key6, key7, key8, key9]).forEach
 
 // click AC btn
 resetBtn.onclick = () => {
-    resText = "0"
-    res = 0;
-    prevRes = undefined;
-    count = 0;
-
-    resetOperators()
-    renderResult()
+    handleResetCalculator()
 }
 
 
 [addBtn, subtractBtn, divideBtn, multiplyBtn].forEach(ele => {
     ele.addEventListener("click", (e) => {
+        if (res == 0) return
         let isActive = e.target.classList.contains("active")
         console.log('active: ', isActive)
         resetOperators()
@@ -113,10 +130,11 @@ resetBtn.onclick = () => {
             prevRes = res
             res = 0
             resText = res.toString()
-            console.log("ok", res)
+            // console.log("ok", res)
         }
 
         if (count > 0 && operator && prevOperator) calcRes()
+        isLastPressCalc = false
 
 
     })
@@ -124,4 +142,48 @@ resetBtn.onclick = () => {
 
 calcBtn.addEventListener("click", () => {
     calcRes()
+})
+
+// handle decimal
+decimalBtn.addEventListener("click", () => {
+    if (isDecimalPressed) return;
+
+    if (isLastPressCalc) {
+        handleResetCalculator()
+    }
+
+    // console.log(res, resText)
+    if (resText == '-') {
+        resText = '-0.'
+    } else {
+        resText = resText + '.'
+    }
+    // console.log(res, resText)
+    renderResult()
+    isDecimalPressed = true
+})
+
+// handle Change sign
+
+changeSignBtn.addEventListener("click", () => {
+    if (isLastPressCalc) {
+        handleResetCalculator()
+    }
+    if (res == 0) {
+        if (!resText.includes("-")) {
+            resText = '-'
+        } else {
+            resText = 0
+        }
+    } else {
+        if (!resText.includes("-")) {
+            resText = '-' + resText
+        } else {
+            resText = resText.slice(1)
+        }
+    }
+
+    res = parseFloat(resText)
+    renderResult()
+
 })
