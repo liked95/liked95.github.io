8bccf604346f1f2459cd6c532ee7121b1c56450f
liked95 <101644745+liked95@users.noreply.github.com>
2022-12-07 16:47:03 +0700

    basic crud client trello

---
+++ bravebits/react-lifecycle/src/components/Panel.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function Panel({ title, children, isActive, onShow }) {
+
+    return (
+        <section className='panel'>
+            <h3>{title}</h3>
+            {isActive ? (
+                <p>{children}</p>
+            ) : (
+                <button onClick={() => onShow()}>Show</button>
+            )}
+        </section>
+    )
+}
+
+export default Panel
